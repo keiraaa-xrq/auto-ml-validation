bd81184427eecb9d1d1c19c5efa70e55e65b6ad3
kk-hue <70795164+kk-hue@users.noreply.github.com>
2023-03-16 17:24:07 +0800

    commit whatever to pull from the latest main

--- auto_ml_validation/app/pages/results.py
+++ auto_ml_validation/app/pages/results.py
@@ -8,17 +8,8 @@ results_layout = html.Div(children=[
     html.Div(children='''
         This is our Result page content.
     '''),
-    html.Div([
-        # New Heading
-        html.Div([
-            html.H5('Probability Stability Index', style={'textAlign': 'left', 'fontWeight': 'bold'})
-        ], style={'backgroundColor': '#ffaf77', 'width': '100%', 'top': 0, 'left': 0, 'margin': 0}),
-
-        html.Div([
-            html.H5('PSI Score', style={'textAlign': 'left', 'fontWeight': 'bold'})
-        ], style={'backgroundColor': '#ffaf77', 'width': '100%', 'top': 0, 'left': 0, 'margin': 0}),
     ])
 
-])
+    
 
 
