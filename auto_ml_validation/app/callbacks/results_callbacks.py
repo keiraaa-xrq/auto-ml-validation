67304bfda6a5100326dcdb8bb45c966c9045bad1
kk-hue <70795164+kk-hue@users.noreply.github.com>
2023-03-26 15:23:54 +0700

    finished layout for psi table and ft metrics by integrate with latest statistical metrics+fixed column display issue in psi and csi table

--- auto_ml_validation/app/callbacks/results_callbacks.py
+++ auto_ml_validation/app/callbacks/results_callbacks.py
@@ -3,8 +3,7 @@ from auto_ml_validation.app.pages.results import *
 from dash.dependencies import Input, Output, State
 import json
 # Body Content
-
-
+    
 # Layout
 results_layout = html.Div([
     results_layout,
--- auto_ml_validation/app/pages/results.py
+++ auto_ml_validation/app/pages/results.py
@@ -1,6 +1,7 @@
 import dash
 from dash import html, dcc, dash_table
 import pandas as pd
+import numpy as np
 from auto_ml_validation.validation_package.evaluation import statistical_metrics_evaluator
 
 
@@ -10,28 +11,81 @@ train_prob = pd.read_csv('././data/stage_2/loanstats_train_proba.csv')
 train_processed = pd.read_csv('././data/stage_2/loanstats_train_processed.csv')
 test_prob = pd.read_csv('././data/stage_2/loanstats_test_proba.csv')
 test_processed = pd.read_csv('././data/stage_2/loanstats_test_processed.csv')
-# prepare the processed dataset
-train_processed['probability'] = train_prob['probability']
-test_processed['probability'] = test_prob['probability']
 
+# load the unprocessed dataset
 test_set = pd.read_csv('././data/stage_2/loanstats_2019Q1_test.csv', index_col = False)
 train_set = pd.read_csv('././data/stage_2/loanstats_2019Q1_train.csv', index_col = False)
+
+all_feature_list = ['loan_amnt', 'int_rate', 'installment', 'home_ownership', 'annual_inc',
+       'verification_status','dti', 'delinq_2yrs',
+       'inq_last_6mths', 'open_acc', 'pub_rec', 'revol_bal', 'total_acc',
+       'initial_list_status', 'out_prncp', 'out_prncp_inv', 'total_pymnt',
+       'total_pymnt_inv', 'total_rec_prncp', 'total_rec_int',
+       'total_rec_late_fee', 'last_pymnt_amnt', 'collections_12_mths_ex_med',
+       'application_type', 'tot_coll_amt', 'tot_cur_bal', 'open_acc_6m',
+       'open_act_il', 'open_il_12m', 'open_il_24m', 'mths_since_rcnt_il',
+       'total_bal_il', 'il_util', 'open_rv_12m', 'open_rv_24m', 'max_bal_bc',
+       'all_util', 'total_rev_hi_lim', 'inq_fi', 'total_cu_tl', 'inq_last_12m',
+       'acc_open_past_24mths', 'avg_cur_bal', 'bc_open_to_buy', 'bc_util',
+       'delinq_amnt', 'mo_sin_old_il_acct', 'mo_sin_old_rev_tl_op',
+       'mo_sin_rcnt_rev_tl_op', 'mo_sin_rcnt_tl', 'mort_acc',
+       'mths_since_recent_bc', 'mths_since_recent_inq',
+       'num_accts_ever_120_pd', 'num_actv_bc_tl', 'num_actv_rev_tl',
+       'num_bc_sats', 'num_bc_tl', 'num_il_tl', 'num_op_rev_tl',
+       'num_rev_accts', 'num_rev_tl_bal_gt_0', 'num_sats',
+       'num_tl_90g_dpd_24m', 'num_tl_op_past_12m', 'pct_tl_nvr_dlq',
+       'percent_bc_gt_75', 'pub_rec_bankruptcies', 'tax_liens',
+       'tot_hi_cred_lim', 'total_bal_ex_mort', 'total_bc_limit',
+       'total_il_high_credit_limit', 'hardship_flag', 'debt_settlement_flag']
+
+train_data, test_data = {}, {}
+
+train_data['raw_X'] = train_set[all_feature_list]
+# print(train_data['raw_X'].columns)
+# Index(['loan_amnt', 'int_rate', 'installment', 'home_ownership', 'annual_inc',
+#        'verification_status', 'dti', 'delinq_2yrs', 'inq_last_6mths',
+#        'open_acc', 'pub_rec', 'revol_bal', 'total_acc', 'initial_list_status',
+#        'out_prncp', 'out_prncp_inv', 'total_pymnt', 'total_pymnt_inv',
+#        'total_rec_prncp', 'total_rec_int', 'total_rec_late_fee',
+#        'last_pymnt_amnt', 'collections_12_mths_ex_med', 'application_type',
+    #    'tot_coll_amt', 'tot_cur_bal', 'open_acc_6m', 'open_act_il',
+    #    'open_il_12m', 'open_il_24m', 'mths_since_rcnt_il', 'total_bal_il',
+    #    'il_util', 'open_rv_12m', 'open_rv_24m', 'max_bal_bc', 'all_util',
+    #    'total_rev_hi_lim', 'inq_fi', 'total_cu_tl', 'inq_last_12m',
+    #    'acc_open_past_24mths', 'avg_cur_bal', 'bc_open_to_buy', 'bc_util',
+    #    'delinq_amnt', 'mo_sin_old_il_acct', 'mo_sin_old_rev_tl_op',
+    #    'mo_sin_rcnt_rev_tl_op', 'mo_sin_rcnt_tl', 'mort_acc',
+    #    'mths_since_recent_bc', 'mths_since_recent_inq',
+    #    'num_accts_ever_120_pd', 'num_actv_bc_tl', 'num_actv_rev_tl',
+    #    'num_bc_sats', 'num_bc_tl', 'num_il_tl', 'num_op_rev_tl',
+    #    'num_rev_accts', 'num_rev_tl_bal_gt_0', 'num_sats',
+    #    'num_tl_90g_dpd_24m', 'num_tl_op_past_12m', 'pct_tl_nvr_dlq',
+    #    'percent_bc_gt_75', 'pub_rec_bankruptcies', 'tax_liens',
+    #    'tot_hi_cred_lim', 'total_bal_ex_mort', 'total_bc_limit',
+    #    'total_il_high_credit_limit', 'hardship_flag', 'debt_settlement_flag']
+train_data['processed_X'] = train_processed.drop(['loan_status'], axis=1)
+train_data['y'] = train_processed['loan_status']
+train_data['pred_proba'] = np.array([1-train_prob['probability'], train_prob['probability']])
+
+test_data['raw_X'] = test_set[all_feature_list]
+test_data['processed_X'] = test_processed.drop(['loan_status'], axis=1)
+test_data['y'] = test_processed['loan_status']
+test_data['pred_proba'] = np.array([1-test_prob['probability'], test_prob['probability']])
 ############## end of codes used to load dummy data, remove later #######################
 
 def statistical_model_metrics_layout(train_data: pd.DataFrame,
                test_data: pd.DataFrame,
                number_of_bins: int) -> html.Div:
     
-    my_class = statistical_metrics_evaluator.StatisticalMetricsEvaluator(train_set, 
-                                                                         test_set)
-    
+    my_class = statistical_metrics_evaluator.StatisticalMetricsEvaluator(train_data, 
+                                                                         test_data)
     psi_score, psi_df = my_class.calculate_psi(number_of_bins)
     # reset index as columns for display
     psi_df.columns = psi_df.columns.astype(str)
     psi_df = psi_df.reset_index()
-    psi_df['probability'] = psi_df['probability'].astype(str)
-    ks_score = my_class.kstest('probability')
-
+    psi_df['index'] = psi_df['index'].astype(str)
+    psi_df.rename(columns = {'index':'ranges'}, inplace = True)
+    ks_score = my_class.kstest('tot_cur_bal')
 
     if ks_score.pvalue <= 0.05:
         ks_string = str(ks_score.pvalue) + '. ' + ' Null hypothese (Training and Testing Set are from the same statistical distribution) is rejected.'
@@ -54,15 +108,13 @@ def statistical_model_metrics_layout(train_data: pd.DataFrame,
     )
 
 
-def feature_metrics_layout(train_set: pd.DataFrame,
-                           test_set: pd.DataFrame,
-                            processed_train: pd.DataFrame,
-                            processed_test: pd.DataFrame,
-                            ft_name_list: list[str],
-                            number_of_bins: int)->html.Div:
+def feature_metrics_layout(train_data: pd.DataFrame,
+                           test_data: pd.DataFrame, 
+                           ft_name_list: list[str], 
+                           number_of_bins: int)->html.Div:
     
-    stats_class = statistical_metrics_evaluator.StatisticalMetricsEvaluator(train_set, 
-                                                                            test_set)
+    stats_class = statistical_metrics_evaluator.StatisticalMetricsEvaluator(train_data, 
+                                                                            test_data)
     
     csi_df, csi_dict = stats_class.csi_for_all_features(ft_name_list, number_of_bins)
 
@@ -71,7 +123,9 @@ def feature_metrics_layout(train_set: pd.DataFrame,
     for df, ft_name in zip(csi_df, ft_name_list):
         df.columns = df.columns.astype(str)
         df = df.reset_index()
-        df[ft_name] = df[ft_name].astype(str)
+        df['index'] = df['index'].astype(str)
+        df.rename(columns = {'index':'ranges'}, inplace = True)
+        csi_children.append(html.Br())
         csi_children.append(html.Br())
         csi_children.append(html.H5(ft_name + ' CSI Score:    ' + str(csi_dict[ft_name]),
                                     style={'textAlign': 'center', 'fontWeight': 'bold'}))
@@ -94,21 +148,19 @@ def feature_metrics_layout(train_set: pd.DataFrame,
         ], style={'width': '100%', 'top': 0, 'left': 0, 'margin': 0}),
         html.Div(csi_children),
         html.Br(),
-        html.Br()
-        
+        html.Br()        
     ]) 
 
-
-    
 
 results_layout = html.Div(children=[
     html.Br(),
     html.Br(),
     html.Br(),
-    statistical_model_metrics_layout(train_set, test_set, train_processed, test_processed, 'probability', 10),
+    statistical_model_metrics_layout(train_data, test_data, 10),
     html.Br(),
     html.Br(),
-    feature_metrics_layout(train_set, test_set, train_processed, test_processed, 
-                           ['loan_amnt', 'int_rate','installment','annual_inc'], 10)
+    feature_metrics_layout(train_data, test_data, 
+                           ['loan_amnt', 'int_rate', 'installment',], 
+                           10)
     ])
 
