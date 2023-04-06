3ed4f4881159fe8228b68aa0145a630402437809
ArizonaTay <gratzxy@gmail.com>
2023-04-06 13:35:11 +0800

    Update transparency_metrics_evaluator.py

--- auto_ml_validation/validation_package/evaluation/transparency_metrics_evaluator.py
+++ auto_ml_validation/validation_package/evaluation/transparency_metrics_evaluator.py
@@ -13,33 +13,31 @@ class TransparencyMetricsEvaluator:
     def __init__(self, model, X, class_name_list=None):
         """
         Initializes the TransparencyMetricsEvaluator object.
+
         Args:
             model (object): The machine learning model to evaluate interpretability for.
             X (pandas.DataFrame): The input data for the machine learning model.
             class_name_list (list, optional): A list of class names. Defaults to None.
         """
-        sample_X = X.sample(n=int(X.shape[0]/100))
-
         self.model = model
-        self.X = sample_X
+        self.X = X
         self.class_name_list = class_name_list
-
-
         
         # Initialize the LIME explainer
         self.lime_explainer = LimeTabularExplainer(
-            sample_X.values,
-            feature_names=sample_X.columns,
+            X.values,
+            feature_names=X.columns,
             discretize_continuous=False,
             class_names=[class_name_list] if class_name_list is not None else None
         )
         
         # Initialize the SHAP explainer
-        self.shap_explainer = shap.Explainer(model.predict, sample_X)
+        self.shap_explainer = shap.Explainer(model.predict, X)
     
     def lime_interpretability(self):
         """
         Calculates LIME interpretability metrics.
+
         Returns:
             tuple: A tuple containing local and global LIME plots, and local and global LIME feature importances.
         """
@@ -47,24 +45,18 @@ class TransparencyMetricsEvaluator:
         predict_fn = lambda x: self.model.predict_proba(x).astype(float)
         local_lime_fig = self.lime_explainer.explain_instance(self.X.iloc[i], predict_fn, num_features=10).as_pyplot_figure()
         local_lime_fig.gca().set_title('')
-
         sp_obj = submodular_pick.SubmodularPick(self.lime_explainer, self.X.values, predict_fn, sample_size=5, num_features=10, num_exps_desired=1).sp_explanations[0]
 
-        
         if 0 in sp_obj.as_map():
             global_lime_fig = sp_obj.as_pyplot_figure(label = 0)
         else:
             global_lime_fig = sp_obj.as_pyplot_figure(label = 1)
- 
-
         global_lime_fig.gca().set_title('')
 
         local_text = self.lime_explainer.explain_instance(self.X.iloc[i], predict_fn, num_features=len(self.X.columns))
+        global_text = submodular_pick.SubmodularPick(self.lime_explainer, self.X.values, predict_fn, sample_size=5, num_features=len(self.X.columns), num_exps_desired=1).sp_explanations[0]
 
-        global_text = submodular_pick.SubmodularPick(self.lime_explainer, self.X.values, predict_fn, sample_size=10, num_features=len(self.X.columns), num_exps_desired=1).sp_explanations[0]
-
-        
-        if 0 in sp_obj.as_map():
+        if 0 in global_text.as_map():
             global_res = [(self.X.columns[feature], weight) for feature, weight in global_text.as_map()[0]]
         else:
             global_res = [(self.X.columns[feature], weight) for feature, weight in global_text.as_map()[1]]        
@@ -76,6 +68,7 @@ class TransparencyMetricsEvaluator:
     def shap_interpretability(self):
         """
         Calculates SHAP interpretability metrics.
+
         Returns:
             tuple: A tuple containing local and global SHAP plots, and local and global SHAP feature importances.
         """
